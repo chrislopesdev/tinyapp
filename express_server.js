3cd0a4b26a07343c18bdfc4a53cdd01d14357269
chris lopes <chris.lopes@gmail.com>
2021-12-06 22:15:32 +0000

    install ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = 8080; // default port 8080
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
@@ -21,14 +22,14 @@ app.get('/hello', (req, res) => {
 });
 
 // variables set in one app.get statement are not available in another
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
+// app.get('/set', (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
 // variable a is not accessible from the previous app.get
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
-});
+// app.get('/fetch', (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
