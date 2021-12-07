5526b9581995f6a4899a13e019868d918cec2b3d
chris lopes <chris.lopes@gmail.com>
2021-12-07 01:39:17 +0000

    add post functionality

--- express_server.js
+++ express_server.js
@@ -1,9 +1,20 @@
-// const { Template } = require('ejs');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const generateRandomString = () => {
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    const randomCharCode = Math.floor(Math.random() * 26 + 97);
+    const randomChar = String.fromCharCode(randomCharCode);
+    randomString += randomChar;
+  }
+  return randomString;
+};
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
@@ -31,11 +42,15 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
-
 
 // variables set in one app.get statement are not available in another
 // app.get('/set', (req, res) => {
