0aaad1d5d78d688a225c31cce61d0e2738a87a4f
chris lopes <chris.lopes@gmail.com>
2021-12-07 12:14:20 +0000

    added comments

--- express_server.js
+++ express_server.js
@@ -6,6 +6,7 @@ const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// generate random string for short url
 const generateRandomString = () => {
   let randomString = '';
   // eslint-disable-next-line no-plusplus
@@ -17,6 +18,7 @@ const generateRandomString = () => {
   return randomString;
 };
 
+// temp database object
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
@@ -30,24 +32,24 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
+// add post route to receive form submission
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+// add route to show url submit form
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+// redirect short urls
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+// redirect after form submission
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
@@ -55,20 +57,12 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+// key: req.params.shortURL
+// value: urlDatabase[req.params.shortURL]
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
-
-// variables set in one app.get statement are not available in another
-// app.get('/set', (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-// variable a is not accessible from the previous app.get
-// app.get('/fetch', (req, res) => {
-//   res.send(`a = ${a}`);
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
